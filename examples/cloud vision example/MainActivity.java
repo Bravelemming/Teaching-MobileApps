4e7fb75b7fce8c1418236beb4f3b67033f73dce2
bravelemming <bravelemming@gmail.com>
2018-02-26 01:19:27 -0800

    P3 google vision api call.  status: crashing.

--- examples/cloud vision example/MainActivity.java
+++ examples/cloud vision example/MainActivity.java
@@ -1,4 +1,20 @@
-package com.kinne.jack.jkp3;
+/*
+ * Copyright 2016 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.sample.cloudvision;
 
 import android.Manifest;
 import android.content.DialogInterface;
@@ -44,7 +60,7 @@ import java.util.List;
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
-    private static final String CLOUD_VISION_API_KEY = "AIzaSyD5UXhILEFjEMol189wgnkd4c8MJfp1Hag";
+    private static final String CLOUD_VISION_API_KEY = "YOUR_API_KEY";
     public static final String FILE_NAME = "temp.jpg";
     private static final String ANDROID_CERT_HEADER = "X-Android-Cert";
     private static final String ANDROID_PACKAGE_HEADER = "X-Android-Package";
